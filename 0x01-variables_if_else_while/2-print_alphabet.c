411b4ab59ca1088a7508bedf2ea1807c02357554
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-18 00:51:25 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main- prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar ('\n');
+	return (0);
+}
+
