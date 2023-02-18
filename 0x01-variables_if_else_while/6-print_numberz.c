69122b55825281104a1bed5dd608ba6a1cce7633
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-18 09:33:12 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main- prints numbers 0 - 9
+ *
+ *Return: end with value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
