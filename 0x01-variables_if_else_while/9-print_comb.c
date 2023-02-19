af13b08dd1d5ce12bf81807eb5d14ac4f77b598f
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-18 19:20:37 -0800

    Single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- prints all possible combinations of single-digit numbers
+ *
+ * Return: end with value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar((char)i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
