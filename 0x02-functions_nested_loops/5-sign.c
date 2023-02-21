cd6e6ade75443f63e3d7cae98c87f279a480449d
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-21 13:51:59 -0800

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign- prints sign of a number
+ *
+ * @n: input character of type int
+ * Return: the sign of a number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
