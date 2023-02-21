4bd5157296d9ac99679db54cc183d89afbd32cab
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-21 14:41:17 -0800

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit- prints last digit of a number
+ *
+ * @n: parameter of type int
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
