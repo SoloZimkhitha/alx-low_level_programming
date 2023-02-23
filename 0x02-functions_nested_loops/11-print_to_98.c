5194d850d5e89c1fcd0b5241bda12e7a78cfd477
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-22 19:28:42 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98- print numbers to 98
+ *@n:starting point
+ * Return: end with value 0
+ */
+void print_to_98(int n)
+{
+	for (int i = n; i <= 98; i++)
+	{
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+	}
+}
