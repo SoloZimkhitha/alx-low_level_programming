042abe218508fa6759104ce791b44da78689b203
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-24 16:48:56 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * print_line- draws a straight line in the terminal
- *
+ *@n: parameter of type int
  * Return: 0
  */
 void print_line(int n)
@@ -11,7 +11,8 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		_putchar('_');	
+		if (n != 0)
+			_putchar('_');
 	}
 	_putchar('$');
 	_putchar('\n');
