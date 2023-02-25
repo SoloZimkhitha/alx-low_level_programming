a11be5051400e90192c6ce1aa23afe2cde432eed
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-24 19:18:21 -0800

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal- draws diagonal line
+ *
+ * @n: parameter of type integer
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+        int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	}
+	{
+	for (j = 0; j <= i; j++)
+	}
+		_putchar(32);
+		_putchar(92);
+		_putchar('\n');
+}
