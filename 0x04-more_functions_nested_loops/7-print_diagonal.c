3f71a3e2629c7c842bb2bd0a3e35aa9e7d210254
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-24 23:59:13 -0800

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,26 @@
 void print_diagonal(int n)
 {
 	int i;
-	int j;
+    	int j;
+    
+   	if (n <= 0)
+   	{
+        	_putchar('\n');
+    	}
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
 	for (i = 0; i < n; i++)
-	{
-	for (j = 0; j <= i; j++)
-	{
-		_putchar(32);
-		_putchar(92);
-		_putchar('\n');
-	}
-	}
-	}
+    	{
+        	for (j = 0; j <= i; j++)
+        	{
+            		if (j == i)
+            		{
+                		_putchar(92);
+            		}
+            		else
+            		{
+                		_putchar(32);
+            		}
+        	}
+        	_putchar('\n');
+    	}
 }
