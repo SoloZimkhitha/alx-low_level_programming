817ce07dcf09e5213f5f9ba5f82c39b76170d3ab
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-27 12:55:52 -0800

    battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98- function that takes a pointer to an int as parameter
- * @*n: parameter of type int pointer
+ * @n: parameter of type int pointer
  * Return: nothing
  */
 void reset_to_98(int *n)
