b23a9a2e8919f6d87c87cb4f634a73a10de537ff
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-27 14:37:21 -0800

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *int_strlen - returns the length of a string
+ *
+ * @s: parameter of type character
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
+
+
