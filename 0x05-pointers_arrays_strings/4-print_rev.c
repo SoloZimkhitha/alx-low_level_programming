a8c867577d3216d61f2c9cb5e0fc70e1093f804b
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-27 15:39:43 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints string in reverse
+ * @s: parameter of type character
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
