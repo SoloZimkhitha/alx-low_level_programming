dfa50a4cb49bdb1a6e9d659115d04e22d4266431
SoloZimkhitha <solozimkhitha@gmail.com>
2023-02-28 00:48:56 -0800

    prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints the second half of the string
+ *
+ * @str: parameter of type character
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+	int half_len = len / 2;
+	int n = (len - 1) / 2;
+
+	for (i = 0; i < half_len; i++)
+	{
+		_putchar(str[i]);
+	}
+		_putchar ('\n');
+		if (len % 2 == 1)
+	{
+			half_len = (len - 1) / 2;
+			half_len = n;
+	}
+		_putchar(str[n]);
+}
