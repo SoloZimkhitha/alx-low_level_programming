e6e9e5e45ab0816d2e8d229a0a336807359143e5
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-01 09:18:57 -0800

    stringcopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 /**
-*
 *_strncpy - copies a string
 *@dest: parameter of type character
 *@src: parameter of type character
@@ -12,5 +11,5 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
-	return(dest);
+	return (dest);
 }
