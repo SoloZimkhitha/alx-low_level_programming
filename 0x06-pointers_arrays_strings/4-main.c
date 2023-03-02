b5e45c7678385eb28e4f8ae85bfd2cedc44cb2d5
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-01 19:06:13 -0800

    reverse

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -5,8 +5,8 @@
  * main - check the code
  * @a: an array of integers
  * @n: the number of elements to swap
- *
- * Return: nothing.
+ * @print_array: parameter of type array
+ * Return: nothing
  */
 void print_array(int *a, int n)
 {
@@ -23,4 +23,11 @@ void print_array(int *a, int n)
         i++;
     }
     printf("\n");
+}
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+    print_array(a, (sizeof(a) / sizeof(int));
+    reverse_array(a, (sizeof(a) / sizeof(int));
+    print_array(a, (sizeof(a) / sizeof(int));
+    return (0);
 }
