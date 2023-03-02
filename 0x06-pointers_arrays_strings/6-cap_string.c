9d14aea623b6e9a87f266ec5cf490203af4ae9e8
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-02 12:50:44 -0800

    capitalise

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: parameter of type character
+ * Return: 0
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	while (c[i] != '\0')
+	{
+		if (islower(c[i]))
+		{
+			c[i] = toupper(c[i]);
+		}
+		i++;
+	}
+	return (c);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * char *leet -  encodes a string
+ *
+ * Return: 0
+ */
+char *leet(char *)
+{
