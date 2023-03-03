2e43ef6f66e7e2395ae5f38e932af62120ba0f19
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-03 01:08:45 -0800

    capitalise

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,5 +6,25 @@
  *
  * Return: 0
  */
-char *leet(char *)
+char *leet(char *c)
 {
+	char chars[5][3] = {
+        {'a', 'A', '4'},
+        {'e', 'E', '3'},
+        {'i', 'I', '1'},
+        {'o', 'O', '0'},
+        {'s', 'S', '5'}};
+
+	for (int i = 0; i < strlen(c); i++)
+	{
+        	for (int j = 0; j < 5; j++)
+		{
+            		if (c[i] == chars[j][0] c[i] == chars[j][1])
+			{
+                		c[i] = chars[j][2];
+                		break;
+            		}
+        	}
+	}
+	return (c);
+}
