7132655b8039ef4c8a49c132db838238b0c80d79
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-06 14:18:45 -0800

    string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,15 @@
 #include <string.h>
 /**
  * _strstr -  locates a substring
- *
- *
+ * @haystack: parameter of type character
+ * @needle: parameter of type character
  * Return: 0
  */
+char *_strstr(char *haystack, char *needle)
+{
+	char *result;
+	
+	result = strstr(haystack, needle);
+	return (result);
+}
+
