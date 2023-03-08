be18412c04d7d20abcde59f68a73100adda24f94
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-08 08:43:04 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -8,15 +9,6 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-	}
-	else
-	{
-		putchar(*s);
-	}
-	_puts_recursion(s + 1);
-	_puts_recursion("Zimkhitha Solo is here to stay baby!");
-	_puts_recursion("");
+	puts(s);
+	_putchar('\n');
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: parameter of type character
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+
+
