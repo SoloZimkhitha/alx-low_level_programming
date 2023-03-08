94d49fc00281b7d4e938f7709f0e0c9fccb59604
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-08 19:42:16 +0200

    Strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+    int i;
+
+    if(*s == '\0')
+    {
+        return i;
+    }
+
+    i++;
+    _strlen_recursion(s);
+}
