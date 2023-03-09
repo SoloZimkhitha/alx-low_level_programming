3a86f676968cf272c6c8ae30f68c4890b91cd1bf
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-08 17:56:40 -0800

    prime fixed2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
 /**
- * prime_num -  that returns 1 if prime number otherwise 0
+ * prime_num - returns 1 if prime number otherwise 0
  * @n: parameter of type integer
  * @i: parameter of type integer
  * Return: 1 or 0
 */
-
 int prime_num(int n, int i)
 {
-    if (n == i)
-    return (1);
-    if (n % i == 0)
-    return (0);
-
-    return (prime_num(n, i + 1));
-     
-
+	if (n == i)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (prime_num(n, i + 1));
 }
+
 /**
- * _prime_number -  that returns 1 if prime number  otherwise 0
+ * is_prime_number - returns prime number  otherwise 0
  * @n: parameter of type integer
- * @i: parameter of type integer
  * Return: 1 or 0
 */
-int is_prime_number (int n)
+int is_prime_number(int n)
 {
-    if (n < 0 || n == 1)
-    return (0);
-
-    return (prime_num(n, 2));
+	if (n < 0 || n == 1)
+	{
+		return (0);
+	}
+	return (prime_num(n, 2));
 }
