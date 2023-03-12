6ff91aaf8c9d2cc52d484676e5530a2f9ca34d78
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-12 12:17:57 -0700

    mutliply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  multiplies, followed by a new line
+ * @argc: parameter of type integer
+ * @argv: parameter of type character
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, r;
+
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	r = x * y;
+	printf("%d\n", r);
+	return (0);
+}
