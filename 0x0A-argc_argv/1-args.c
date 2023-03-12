b62236756def7a407707cad65443506c77fb09a9
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-12 11:39:15 -0700

    betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,13 @@
 
 /**
  * main -  prints a number followed by a new line
- * argc: parameter of type integer
- * argv: parameter of type character
+ * @argc: parameter of type integer
+ * @argv: parameter of type character
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
-        printf("%d\n", argc - 1);
-        return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
