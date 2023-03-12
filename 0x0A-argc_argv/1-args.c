f343675517a1a38f4b464655cb48d321730e1d07
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-12 10:47:06 -0700

    print number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,12 +9,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	printf("argc = %d\n", argc);
-	for (i = 0; i < argc; i++)
-		{
-		printf("argv[%d] = %s\n", i, argv[i]);
-		}
+	printf("argc = %d\n", argc - 1);
 	return (0);
 }
