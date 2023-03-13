9edd317578ea54f9670bc9c5b7f51230f5f0160d
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-13 13:38:58 -0700

    char arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,16 +7,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *arr;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	arr = malloc(size * sizeof(char));
+	arr = (char*) malloc(size * sizeof(char));
 	if (arr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
 	}
 	arr[0] = c;
 	return (arr);
