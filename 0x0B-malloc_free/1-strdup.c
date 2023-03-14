254acca83677197e781878f77fb2413c16665b74
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-14 14:37:02 -0700

    stry copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - eturns a pointer to a new string
+ * @str: parameter of type char
+ * Return: a pointer to a new string
+ */
+char *_strdup(char *str)
+{
+	unsigned int len;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = strlen(str) + 1;
+	arr = malloc(len * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(arr, str, len);
+	return (arr);
+}
