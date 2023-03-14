04553244a663e6c14db9fd4de7ca6ef5af0b5fac
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-14 15:09:49 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat -  copies a string
+ * @s1: parameter of type integer
+ * @s2: parameter of type character
+ * Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1;
+	unsigned int len2;
+	char *concat;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(concat, s1, len1);
+	memcpy(concat + len1, s2, len2 + 1);
+
+	return (concat);
+}
+
