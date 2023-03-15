7b367e84365aba1f41f730a3863c6ede50cd518a
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-15 12:25:04 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: parameter of type integer
+ * @height: parameter of type integer
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}	
+	free(grid);
+	
+}
