dc078228f5b660f2b9194b8fb35f9f1eb3e4299e
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-17 16:59:24 -0700

    two strings fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,9 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	}
 	if (s2 == NULL)
-        {
-                s2 = "";
-        }
+	{
+		s2 = "";
+	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	if (n >= len2)
