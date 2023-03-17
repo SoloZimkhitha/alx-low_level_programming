f6ef4fe35f05c0129c5a5aa9af59e1cb9f0e8277
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-17 16:38:22 -0700

    two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,30 +10,26 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1, len2;
-    char *result;
-  
-    if (s1 == NULL)
-    {
-        s1 = " ";
-    }
-    
-   len1 = strlen(s1);
-   len2 = strlen(s2);
+	unsigned int len1, len2;
+	char *result;
 
-    if (n >= len2)
-        {
-            n = len2;
-        }
-  
-    result = (char *)malloc(len1 + n + 1);   
-    if (result == NULL) 
-        {
-            return NULL;
-       }
-
-    strncpy(result, s1, len1);
-    strncpy(result + len1, s2, n);
-    result[len1 + n] = '\0';
-    return result;
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	result = (char *)malloc(len1 + n + 1);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	strncpy(result, s1, len1);
+	strncpy(result + len1, s2, n);
+	result[len1 + n] = '\0';
+	return (result);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,9 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-<<<<<<< HEAD
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
-=======
->>>>>>> bd827878b4d5d569275d26b8209b6fe62b891e9f
