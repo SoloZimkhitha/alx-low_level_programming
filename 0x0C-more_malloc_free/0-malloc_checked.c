78b0cd28b493664a21bdae8ba7bae068b5ab1340
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-17 18:43:17 -0700

    malloc failed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,44 +1,16 @@
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 #include <stdio.h>
 /**
  * malloc_checked - allocates memory using malloc
- * unsigned b - a parameter of type integer
+ * @b: parameter of type integer
  * Return: a void pointer
 */
 void *malloc_checked(unsigned int b)
 {
     void *ptr = malloc(b);
-    if (ptr = ('\0'));
+    if (ptr == NULL)
     {
-        putchar('E');
-        putchar('r');
-        putchar('r');
-        putchar('o');
-        putchar('r');
-        putchar(':');
-        putchar(' ');
-        putchar('m');
-        putchar('a');
-        putchar('l');
-        putchar('l');
-        putchar('o');
-        putchar('c');
-        putchar(' ');
-        putchar('f');
-        putchar('a');
-        putchar('i');
-        putchar('l');
-        putchar('e');
-        putchar('d');
-        putchar('\n');
-        exit(98);
+	    exit(98);
     }
     return (ptr);
-}
-
-     
-
-
 }
