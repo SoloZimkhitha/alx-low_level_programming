078f14f53712c26acc0eeb56b1f9780200733bbc
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-19 14:15:18 -0700

    calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,8 +21,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	size_t len1 = s1 ? strlen(s1) : 0;
+	size_t len2 = s2 ? strlen(s2) : 0;
 	if (n >= len2)
 	{
 		n = len2;
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	memcpy(result, s1, len1);
-	memcpy(result + len1, s2, n);
-	result[len1 + n] = '\0';
+	memcpy(result + len1, s2, len2);
+	result[len1 + len2] = '\0';
 	return (result);
 }
