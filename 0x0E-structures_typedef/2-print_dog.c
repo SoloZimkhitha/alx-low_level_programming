b6fd94d7a934c725eaf16b5c438e48c64ac2d7d5
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-22 14:47:24 -0700

    nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf("nil");
+		printf("(nil)");
                 return;
 	}
     printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
