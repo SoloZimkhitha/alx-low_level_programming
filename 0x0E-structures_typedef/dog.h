1e1adb6356001b8830b7e0d41e01bf69e0b1b5d1
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-22 05:02:25 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,10 @@
  */
 typedef struct dog
 {
-        char *name;
-        float age;
-        char *owner;
-} dog_t;      
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 void init_dog(struct dog *p, char *name, float age, char *owner);
 void print_dog(struct dog *p);
 #endif
