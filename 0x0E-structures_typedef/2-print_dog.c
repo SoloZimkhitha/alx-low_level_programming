cc27c98ba719f0c585ea48776947a37c079bf1eb
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-23 11:23:37 -0700

    print statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,12 @@ void print_dog(struct dog *d)
 	}
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("%s\n", "(nil)");
+	}
 }
