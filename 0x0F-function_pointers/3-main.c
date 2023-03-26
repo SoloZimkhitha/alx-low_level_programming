d4440ef4cc635d0a1cf6a317665c228494a31036
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-27 00:23:09 +0200

    checks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,19 +8,20 @@ int main(int argc, char *argv[])
 	int num1, num2, result;
 	int (*operation)(int, int);
 
-	if (argc != 4)
+	if (argc != 4 || (*argv[2] = NULL && argv[3] == 0))
 	{
-	       	return (1);
+		printf("%s\n", "Error");
+	    return (1);
 	}
 
 	num1 = atoi(argv[1]);
-       	num2 = atoi(argv[3]);
+    num2 = atoi(argv[3]);
 	
 	operation = get_op_func(argv[2]);
 
-	if (operation == NULL)
+	if (operation == NULL || argv[2][1] != '\0')
 	{
-        	printf("Error\n");
+        	printf("%s\n", "Error");
         	return (1);
 	}
 
