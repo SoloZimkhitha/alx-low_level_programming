5fa446bbd4056c3be2683381168f8c2c56250408
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-26 04:18:19 -0700

    search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+/**
+ * int_index - search for an int in an array
+ * @array: paramer of type int pointer
+ * @size: parameter of type int
+ * @cmp: parameter of type function pointer
+ * Return: int.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+        	return -1;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+        	if (cmp(array[i]) != 0)
+		{
+        		return i;
+		}
+	}
+
+	return -1;
+}
