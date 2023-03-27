5d35fbe3cf808ef6d1a4714a2dd14ab35b7d078d
SoloZimkhitha <solozimkhitha@gmail.com>
2023-03-27 00:00:42 -0700

    sum all numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,48 +1,25 @@
-<<<<<<< HEAD
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
-int sum_them_all(const unsigned int n, ...)
-{
-	unsigned int i, sum = 0;
-	va_list args;
-
-	if (n == 0)
-	{
-		return 0;
-	}
-	va_start(args,n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(args, int);
-	}
-	va_end(args);
-	return (sum);
-}
-=======
-#include <stdlib.h>
-#include "variadic_functions.h"
 /**
- * sum_them_all: sums all parameters
- * @n: parameter of type unsigned int
-*/
+ * sum_them_all -  returns the sum of all its parameters
+ * @n: parameter of type n
+ * Return: sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-   unsigned int i, sum;
+        unsigned int i, sum = 0;
+        va_list args;
 
-    if (n == 0)
-    {
-        return(0);
-    }
-
-    sum = 0;
-
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int)
-    }
-    va_end(args);
+        if (n == 0)
+        {
+                return (0);
+        }
+        va_start(args, n);
+        for (i = 0; i < n; i++)
+        {
+                sum += va_arg(args, int);
+        }
+        va_end(args);
         return (sum);
 }
->>>>>>> 9f8b06cb86851972186fa68cf34647258f1fc22b
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
@@ -7,11 +6,3 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #endif
-=======
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-int sum_them_all(const unsigned int n, ...);
-
-#endif
->>>>>>> 9f8b06cb86851972186fa68cf34647258f1fc22b
